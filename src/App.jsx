00318106f07a626c5416f640ec05efb457ec321c
bhanu7410 <bhanuprabhath123@gmail.com>
2025-02-03 23:58:44 +0530

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+	return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+	let primaryDiscription =
+		reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+	return (
+		<header>
+			<img src={reactImg} alt="Stylized atom" />
+			<h1>React Essentials</h1>
+			<p>
+				{primaryDiscription} React concepts you will need for almost any
+				app you are going to build!
+			</p>
+		</header>
+	);
+}
 
 function App() {
-	const handleTimeout = () => console.log("LESSSGOOOOO");
-	const [count, setCount] = useState(0);
-
-	setTimeout(handleTimeout, 2000);
-	setTimeout(() => console.log("we are the one !!"), 4000);
-
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<div>
+			<Header />
+			<main>
+				<h2>Time to get started!</h2>
+			</main>
+		</div>
 	);
 }
 
