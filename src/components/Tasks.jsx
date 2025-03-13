31994b992a3b69a122e61e3514d305034ec79396
bhanu7410 <bhanuprabhath123@gmail.com>
2025-03-14 00:48:59 +0530

    task updated

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { use, useRef, useState } from "react";
 
 import DeleteIconBlack from "../assets/Cross.png";
 
@@ -12,11 +12,17 @@ export default function Tasks({
 }) {
 	const [newTaskStatus, setNewTaskStatus] = useState(false);
 	const [newTaskDetails, setNewTaskDetails] = useState("");
+	const [editTasknumber, setEditTaskNumber] = useState(null);
+	const [editTaskDetails, setEditTaskDetails] = useState(null);
+
+	const inputNewTaskRef = useRef(null);
 
 	function handleNewTask() {
-		setNewTaskStatus((status) => !status);
+		setNewTaskStatus((status) => false);
 		let newSetTasks = [...projectDetails[currentProjectId].tasks];
-		newSetTasks.push(newTaskDetails);
+		newSetTasks.push(
+			newTaskDetails ? newTaskDetails : "Random Task Created",
+		);
 		setProjectDetails((projectArray) => {
 			const newProjects = {
 				...projectArray,
@@ -34,7 +40,6 @@ export default function Tasks({
 	function handleDeleteTask(index) {
 		let newSetTasks = [...projectDetails[currentProjectId].tasks];
 		newSetTasks.splice(index, 1);
-		console.log(index, newSetTasks);
 		setProjectDetails((projectArray) => {
 			const newProjects = {
 				...projectArray,
@@ -53,19 +58,73 @@ export default function Tasks({
 		setNewTaskDetails(() => target);
 	}
 
+	function handleNewTaskClick() {
+		if (newTaskStatus) {
+			handleNewTask();
+			setNewTaskStatus(() => true);
+			inputNewTaskRef.current.focus();
+		} else setNewTaskStatus((status) => !status);
+	}
+
+	function handleEditTaskClick(index) {
+		setEditTaskNumber(() => index);
+	}
+
+	function handleEdittedTask(index) {
+		let newSetTasks = [...projectDetails[currentProjectId].tasks];
+		newSetTasks[index] = editTaskDetails
+			? editTaskDetails
+			: newSetTasks[index];
+		setProjectDetails((projectArray) => {
+			const newProjects = {
+				...projectArray,
+				[currentProjectId]: {
+					...projectArray[currentProjectId],
+					tasks: [...newSetTasks],
+					dateModified: getCurrentDateTime(),
+				},
+			};
+			return newProjects;
+		});
+		setEditTaskNumber(null);
+		setEditTaskDetails(null);
+	}
+
 	return (
-		<div className="pl-4 my-2 mr-4">
+		<div className="my-2 mr-4 pl-4">
 			{tasks.map((task, index) => (
 				<div
 					key={index}
-					className="flex items-center justify-between my-2"
+					className="my-2 flex items-center justify-between"
 				>
-					<div className="flex-auto text-xl text-left border-2 border-transparent rounded cursor-pointer hover:border-sky-400">
-						{index}.{task}
-					</div>
+					{editTasknumber === index ? (
+						<input
+							type="text"
+							name={`task ${index}`}
+							className="w-full rounded-lg p-2 pl-4 text-xl"
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									handleEdittedTask(index);
+								}
+								if (e.key === "Escape") {
+									setEditTaskNumber(null);
+									setEditTaskDetails(null);
+								}
+							}}
+							onChange={(e) => setEditTaskDetails(e.target.value)}
+							autoFocus
+						/>
+					) : (
+						<div
+							onClick={() => handleEditTaskClick(index)}
+							className="flex-auto cursor-pointer rounded border-2 border-transparent text-left text-xl hover:border-sky-400"
+						>
+							{index}.{task}
+						</div>
+					)}
 					<button
 						onClick={() => handleDeleteTask(index)}
-						className="flex-none px-2 py-1 text-xl transition-all duration-100 ease-in rounded cursor-pointer hover:bg-red-400"
+						className="flex-none cursor-pointer rounded px-2 py-1 text-xl transition-all duration-100 ease-in hover:bg-red-400"
 					>
 						<img
 							className="w-6"
@@ -82,16 +141,21 @@ export default function Tasks({
 						if (e.key === "Enter") {
 							handleNewTask();
 						}
+						if (e.key === "Escape") {
+							setNewTaskStatus(false);
+							setNewTaskDetails("");
+						}
 					}}
 					onChange={(event) => handleNewTaskEntry(event.target.value)}
-					className="w-full p-2 pl-4 text-xl rounded-lg"
-					value={newTaskDetails}
+					className="mb-2 w-full rounded-lg p-2 pl-4 text-xl outline-2 outline-blue-500"
+					value={newTaskDetails ? newTaskDetails : ""}
+					ref={inputNewTaskRef}
 				/>
 			) : null}
 
 			<button
-				onClick={() => setNewTaskStatus((status) => !status)}
-				className="w-full p-2 pl-4 text-xl transition-all duration-150 ease-in border-2 border-green-400 rounded-2xl hover:bg-green-400"
+				onClick={() => handleNewTaskClick()}
+				className="w-full rounded-2xl border-2 border-green-400 p-2 pl-4 text-xl transition-all duration-150 ease-in hover:bg-green-400"
 			>
 				+ New Task
 			</button>
