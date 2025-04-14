217c1d802babd0f4318ccc14c6fad1c5031c2830
Bhanu Prabhath <70201186+bhanu7410@users.noreply.github.com>
2025-04-14 17:51:23 +0530

    let's see

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+	return (
+		<>
+			<div className="center block text-5xl font-bold text-black">
+				Hello World
+			</div>
+		</>
+	);
+}
+
+export default App;
